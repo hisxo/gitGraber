e33371aedfef01c51a76013482a91bddf10f6823
Sherlock Secure <52328067+sherlocksecurity@users.noreply.github.com>
2019-11-14 15:16:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
-GITHUB_TOKENS = ['X', 'X', 'X', 'X', 'X']
+GITHUB_TOKENS = ['7e626cccbeb3aac73f44bb82acbdec6606855941', 'aa6a5f6799a22f44a4566bae73fb8d7d9f1dfed9', 'a940067d8f70f545480ae49577dd24780820371b', 'ee554d3331c1e8528b7530829efb24864dc037bc', '38a8b56742296662664db291853905a893c814c8', 'dedc8a75a40451ac7402663f602ae16a5bb465c2', '9702faadf4afdad1075763121925cba2d218ef9c', 'e0e6ced8ef1c1ccb001a4a88db38aa051b1a3f84', '13243d0d7c2950c2d376284dfd26ffa64a17cb40', '99af352812ac38fb5c6dac92fca03b9a132e50b3', '4c0b8a67e2ea49218d06e91da6de9f66dc3afb5e', '09f4b5b0e0e138fe317770d80734ac39a2c4cf68', 'b848efd4b9996a583ae619ad21c2d8e9885c0df1']
 GITHUB_URL_FILE = 'rawGitUrls.txt'
 GITHUB_API_URL = 'https://api.github.com/search/code?q='
+GITHUB_API_COMMIT_URL = 'https://api.github.com/repos/'
 GITHUB_SEARCH_PARAMS = '&sort=indexed&o=desc'
+GITHUB_BASE_URL = 'https://github.com'
+GITHUB_MAX_RETRY = 10
 SLACK_WEBHOOKURL = 'https://hooks.slack.com/services/TXXXXXXXX/BXXXXXXXX/XXXXXXXXXXXXXXXXXXXXXXX'
+TELEGRAM_CONFIG = {
+    "token": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "chat_id": -999999999999999
+}
